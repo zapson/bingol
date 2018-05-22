816108b90d5d3a06488f5c10f9d23ca4b9610824
Marcelo Zapatta <marcelo.zapatta@fatec.sp.gov.br>
2018-05-22 00:24:55 -0300

    Correção da randomização dos números

--- bingol.js
+++ bingol.js
@@ -1,31 +1,41 @@
 // Begin do Bingol.js
 var chubs = [];
+var numeros = [];
 var igual = false;
 var n;
 
 $(function () {
+	for (var i = 1; i < 100; i++) {
+		numeros.push(i);
+	}
+
 	$('#btnRoda').click(function(e){
-		igual = false;
+		igual 	= false;
 
-		// Isto está funcionando, a api html5 utiliza creio que uma espécie de cookie
-		var numeros = {"numeros" : [1, 20, 30, 40]};
+		n = gerarNumero();
+		console.log(n);
+		if(n === 100){
+			alert('acabou');
+			return false;
+		}
 
-		//localStorage.setItem("bingol", JSON.stringify(numeros));
+		chubs.push(numeros[n]);
+
+		numeros.splice(n, 1);
+
+		localStorage.setItem("bingol", JSON.stringify(chubs));
 		console.log(localStorage.getItem("bingol"));
 	});
 
+	/**
+	*	Gera um número aleatório de acordo com o tamanho do array
+	*/
 	function gerarNumero(){
-		numero = Math.floor(Math.random() * 100) + 1;
-		$.each(chubs, function (index, chubinha) {
-			if(chubinha === numero){
-				igual = true;
-			}
-		})
-
-		if(igual == true){
-			gerarNumero();
-		} else {
-			return numero;
+		if(numeros.length === 0){
+			return 100;
 		}
+		numero = Math.floor(Math.random() * numeros.length);
+
+		return numero;
 	}
 });
