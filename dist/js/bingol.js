f5a77cbbd25ce93b9e78db3bd2390e314c51dc2c
Marcelo Zapatta <marcelo.zapatta@fatec.sp.gov.br>
2018-05-23 00:51:00 -0300

    Mudança no layout com implementação do bootstrap

--- dist/js/bingol.js
+++ dist/js/bingol.js
@@ -3,36 +3,43 @@ var chubs = [];
 var numeros = [];
 var igual = false;
 var n;
+// vai criar uma nova instancia no localstorage toda vez que entrar na pagina para armazenar o jogo :D
+var d = new Date();
+var entry = d.toJSON();
 
 $(function () {
-	// vai criar uma nova instancia no localstorage toda vez que entrar na pagina para armazenar o jogo :D
-	var d = new Date();
-	var entry = d.toJSON();
 
 	for (var i = 1; i < 100; i++) {
 		numeros.push(i);
 	}
 
 	$('#btnRoda').click(function(e){
-		if(numeros.length === 0){
-			alert('acabou');
-			return false;
-		}
-
-		n = gerarNumero();
-
-		chubs.push(numeros[n]);
-
-		numeros.splice(n, 1);
-
-		localStorage.setItem(entry, JSON.stringify(chubs));
+		tirarBolinha();
 	});
+
+
 
 	$('#btnBingo').click(function () {
 		
 	})
 
 });
+
+function tirarBolinha(){
+	if(numeros.length === 0){
+		return false;
+	}
+
+	n = gerarNumero();
+
+	$('.bingol-numero').text(n);
+
+	chubs.push(numeros[n]);
+
+	numeros.splice(n, 1);
+
+	localStorage.setItem(entry, JSON.stringify(chubs));
+}
 
 /**
 *	Gera um número aleatório de acordo com o tamanho do array
