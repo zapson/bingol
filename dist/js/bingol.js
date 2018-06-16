49a8cef07da33460e607020cab950ea124e968e4
Marcelo Zapatta <marcelo.zapatta@fatec.sp.gov.br>
2018-06-16 00:04:45 -0300

    Version LTS 6.8.1

--- dist/js/bingol.js
+++ dist/js/bingol.js
@@ -16,7 +16,9 @@ $(function () {
 			tirarBolinha();
 		}
 	});
-	for (var i = 1; i < 100; i++) {
+
+	// Preenche do 1 ao 75
+	for (var i = 1; i <= 75; i++) {
 		numeros.push(i);
 	}
 
@@ -34,29 +36,33 @@ $(function () {
 
 });
 
-if(!stop) {
-	function tirarBolinha(){
+function tirarBolinha(){
+	if(!stop) {
 		if(numeros.length === 0){
 			stop = true;
+			return true;
 		}
  
-		n = gerarNumero();
+		n = gerarNumero(numeros);
 
+		// Printa o numero que saiu na tela
 		$('.bingol-numero').text(n);
-
-		chubs.push(numeros[n]);
-
-		numeros.splice(n, 1);
-
+		chubs.push(n);
 		localStorage.setItem(entry, JSON.stringify(chubs));
+		atualizaNumeros(n);
 	}
 }
 
 /**
-*	Gera um número aleatório de acordo com o tamanho do array
+*	Retorna um valor do array de numeros
+*	e remove esse valor retornando o numero tirado
 */
-function gerarNumero(){
-	numero = Math.floor(Math.random() * numeros.length);
+function gerarNumero(numeros){
+	var indexRandom = Math.floor(Math.random() * numeros.length);
+	var numero 		= numeros[indexRandom];
+
+	// Diz o JS que isso já vai alterar a variavel
+	numeros.splice(indexRandom, 1);
 
 	return numero;
 }
@@ -66,15 +72,19 @@ function gerarNumero(){
  * pula para o próximo!
  */
 function checaCaracter() {
-	var cells = $('.bingol-cell').keydown(function () {
-		if($(this).val().length == 2) {
-			var nextCell = cells.get(cells.index(this) + 1);
-			if(nextCell){
-				nextCell.focus();
-			} else {
-				nextCell = cells.get(cells.index(this) + 2);
+	var cells = $('.bingol-cell').keydown(function (e) {
+		console.log(window.getSelection);
+		// Caso seja diferente de teclas de apagar ou TAB
+		if(e.keyCode !== 8 && e.keyCode !== 46 && e.keyCode !== 9){
+			if($(this).val().length == 2) {
+				var nextCell = cells.get(cells.index(this) + 1);
 				if(nextCell){
 					nextCell.focus();
+				} else {
+					nextCell = cells.get(cells.index(this) + 2);
+					if(nextCell){
+						nextCell.focus();
+					}
 				}
 			}
 		}
@@ -86,7 +96,8 @@ function checaCaracter() {
  * a cartela indicada nos campos
  */
 function validarCampos() {
-
+	$('.bingol-cells').find('input').removeClass('bingol-success');
+	$('.bingol-cells').find('input').removeClass('bingol-error');
 	/**
 	 * Armazena os números marcados da cartela
 	 * já preenchida
@@ -98,12 +109,29 @@ function validarCampos() {
 	 * Variável de controle quando um número 
 	 * está errado
 	 */
-	var erro;
+	var erro = { 
+				i:null,
+	 			valor:null 
+	 			};
+
+
+	/**
+	 * Array para conter todos os erros
+	 */
+	
+	var erros = [];
 
 	/**
 	 * Variável de controle para pular a iteração
 	 */
 	var skip = false;
+
+	/**
+	 * Variável de controle para pular todas as iterações
+	 */
+	var skipAll = false;
+
+	var verdadeiro = false;
 
 	/**
 	 * Percorre todas os campos da cartela
@@ -114,26 +142,62 @@ function validarCampos() {
 		campos.push($(celula));
 	});
 
-	$(campos).each(function (index, campo) {
-		$(chubs).each(function (index, chub) {
+	$(campos).each(function (indexCampo, campo) {
+
+		// Foge do laço
+		// if(skipAll) {
+		// 	return true;
+		// }
+		// console.log('Verificano o campo: ' + indexCampo);
+		verdadeiro = false;
+		$(chubs).each(function (indexChubs, chub) {
+			// console.log('com o elemento: ' + indexChubs);
 			// Verifica se já ocorreu a verificação do
-			// elemento
-			if(skip){
-				return;
-			}
+			// elemento e pula pro próximo
+			// if(skip){
+			// 	return;
+			// }
+
 			// Verifica se é igual a algum elemento
 			// presente em chubs
 			if($(campo).val() == chub){
+				// console.log('campo de index: ' + indexCampo + 'verdadeiro');
 
-				// Se sim pula para o próximo elemento do campo
-				skip = true;
-				return true;
+				// nao seta o erro se for true pq essa porra
+				// n ta funfando com return
+				// index = indexChubs;
+				// return true;
+				verdadeiro = true;
+			} else {
+
+				
+				if(indexChubs == chubs.length - 1){
+					// erro.i = indexCampo;
+					// erro.valor = $(campo).val();
+					// if(index != indexChubs){
+						if(verdadeiro == false){
+							erros.push({i:indexCampo, valor:$(campo).val()});
+						}
+
+					// }
+					// skipAll = true;
+					// return true;
+				}
 			}
-			
 		})
 	})
 
-	console.log(chubs);
-	console.log(localStorage);
-	console.log(localStorage.getItem(entry));
+	$('.bingol-cells').find('input').addClass('bingol-success');
+	$(erros).each(function (index, erro) {
+		$('.bingol-cells').find('#'+erro.i).removeClass('bingol-success');
+		$('.bingol-cells').find('#'+erro.i).addClass('bingol-error');
+	})
+}
+
+function atualizaNumeros(numeros) {
+	// var numeros = localStorage.getItem(entry);
+	$(numeros).each(function (index, value) {
+		$('#numerosForam').append('<span class="btn btn-primary numerinho">' + value + '</span>')
+		// console.log('index: ' + index + 'valor: '+ value)
+	})
 }
